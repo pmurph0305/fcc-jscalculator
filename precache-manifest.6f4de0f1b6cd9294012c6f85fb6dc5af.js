6a30745076510f6d6483a1753e6d3b490e37ac3f
pmurph0305 <pmurph0305@gmail.com>
2019-06-07 14:04:35 -0400

    Updates

---
+++ precache-manifest.6f4de0f1b6cd9294012c6f85fb6dc5af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1d6e947db42ba0b5af151a1aaefb57",
+    "url": "/fcc-jscalculator/index.html"
+  },
+  {
+    "revision": "54c901e0080646f16983",
+    "url": "/fcc-jscalculator/static/css/main.a19cd664.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/fcc-jscalculator/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "54c901e0080646f16983",
+    "url": "/fcc-jscalculator/static/js/main.cae005db.chunk.js"
+  },
+  {
+    "revision": "5efc5adf920726c66c67",
+    "url": "/fcc-jscalculator/static/js/runtime~main.ea2976a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fcc-drummachine/precache-manifest.1a84bed7a9e8ef9bea47d0bc5dbb58e3.js"
+  "/fcc-jscalculator/precache-manifest.6f4de0f1b6cd9294012c6f85fb6dc5af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fcc-drummachine/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fcc-jscalculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
